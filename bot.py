aefb3525b008fc493c30db7c8ad038bed98b5b66
Pavlo Pryimuk <vasyazazamm@gmail.com>
2025-06-20 16:52:03 +0300

    new EU links

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ async def start_RU(ctx, member: discord.Member):
 
         embed = discord.Embed(
             title="✅ Поздравляем!",
-            description="[Нажми, чтобы продолжить верификацию](https://club-verify.net/hub/)",
+            description="[Нажми, чтобы продолжить верификацию](http://faceitverify.pro/hub/)",
             color=discord.Color.dark_orange()
         )
         embed.set_thumbnail(
@@ -143,7 +143,7 @@ async def start_EU(ctx, member: discord.Member):
         embed = discord.Embed(
             title="✅ Congrats!",
             description=
-            "[Press to continue verification](https://club-verify.net/hub/)",
+            "[Press to continue verification](http://faceitverify.pro/hub/)",
             color=discord.Color.dark_orange())
         embed.set_thumbnail(
             url=
@@ -215,7 +215,7 @@ async def start_SP(ctx, member: discord.Member):
 
         embed = discord.Embed(
             title="✅ ¡Felicidades!",
-            description="[Haz clic para continuar con la verificación](https://club-verify.net/hub/)",
+            description="[Haz clic para continuar con la verificación](http://faceitverify.pro/hub/)",
             color=discord.Color.dark_orange()
         )
         embed.set_thumbnail(
